e9154a81ce65887bccb4dec2816b31eda0dcd818
Hans Rasch <hans.rasch@highdeveloper.com>
2019-12-23 14:17:03 -0500

    Se agrega el archivo de configuracion 'config-dev'.

---
+++ src/config/config-dev.ts
@@ -0,0 +1,25 @@
+export const LOG_PATH_ = "/Users/hd4/Documents/MAYASOFT/Logs";
+
+export const DATA_BASE_CONFIG_TEST_ = {
+    host: "localhost",
+    port: 5432,
+    database: "axpress_courier",
+    user: "user_hd",
+    password: "hd",
+    // pool: {
+    max: 50,
+    min: 1,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+    // },
+    // connectionTimeout: 30000
+    // connectionString?: string;
+    //   keepAlive?: boolean;
+    //   stream?: stream.Duplex;
+    //   statement_timeout?: false | number;
+    //   connectionTimeoutMillis?: number;
+    //   keepAliveInitialDelayMillis?: number;
+};
+
+// RUTAS ALMACENAMIENTO DE ARCHIVOS
+export const PATH_DOCUMENT_COMPANY_ = "../../public/register-documents"
